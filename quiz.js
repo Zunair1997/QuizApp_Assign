5e65ddb897f3fc60a78b5ddb730fe3f61a6c2b3f
Muhammad Zuanir khan <khanzunair4471@gmail.com>
2020-08-09 16:52:19 +0500

    QuizApp_Assign

---
+++ quiz.js
@@ -0,0 +1,193 @@
+(function(){
+
+    var data = {
+        quizContent: [
+            {
+                question: " Who was the captain of Pakistan in the 1996 world cup?",
+                answer1: "Moin Khan",
+                answer2: "Wasim Akram",
+                answer3: "Saleem Mailk",
+                correctAnswer: 2
+            },
+            {
+                question: "Who was appointed the coach of the Pakistani Cricket team after the World Cup of 2003?:",
+                answer1: "Majid Khan",
+                answer2: "Fazal Mehmood",
+                answer3: " Javed Miandad",
+                correctAnswer: 3
+            },
+            {
+                question: "Who replaced Waqar Younis as the test and one-day captain after 2003 world cup?",
+                answer1: "Wasim Akram",
+                answer2: "Inzimam-ul-haq",
+                answer3: "Rashid Latif",
+                correctAnswer: 3
+            },
+            {
+                question: "Which of the following players did not play 100 test matches for Pakistan?",
+                answer1: "Imran Khan",
+                answer2: "Javed Miandad",
+                answer3: "Majid Khan",
+                correctAnswer: 1
+            },
+            {
+                question: "Which player is known as Street Fighter?",
+                answer1: "Wasim Akram",
+                answer2: "Javed Miandad",
+                answer3: "Waqar Younus",
+                correctAnswer: 2
+            }
+        ],
+        points: 0
+    };
+    
+    var display = {
+        getApp: document.getElementById('app'),
+
+        // Updates DOM on start/restart of the quiz
+        mainPage: function() {
+            var newEl = '<div class="quest-number"><p id="questNumber"></p></div><h1 id="questionDisplay" class="h3"></h1>';
+                newEl += '<ul><li><label><input type="radio" name="answers" id="input1" value="1"><span class="outer"><span class="inner"></span></span><div id="answerDisplay1"></div></label></li>';
+                newEl += '<li><label><input type="radio" name="answers" id="input2" value="2"><span class="outer"><span class="inner"></span></span><div id="answerDisplay2"></div></label></li>';
+                newEl += '<li><label><input type="radio" name="answers" id="input3" value="3"><span class="outer"><span class="inner"></span></span><div id="answerDisplay3"></div></label></li></ul>';
+                newEl += '<div class="points-wrap"><p id="currentPoints"></p></div>';
+
+            this.getApp.innerHTML = newEl;
+        },
+
+        // Updates DOM with each question
+        updateMainPage: function() {
+            var getQuestNumber = document.getElementById('questNumber'),
+                getQuestion = document.getElementById('questionDisplay'),
+                getAnswer1 = document.getElementById('answerDisplay1'),
+                getAnswer2 = document.getElementById('answerDisplay2'),
+                getAnswer3 = document.getElementById('answerDisplay3'),
+                getCurrentPoints = document.getElementById('currentPoints'),
+                sumOfQuestions = data.quizContent.length;
+                
+            getQuestNumber.innerHTML = control.count + 1 + '/' + sumOfQuestions;
+            getQuestion.innerHTML = data.quizContent[control.count].question;
+            getAnswer1.innerHTML = data.quizContent[control.count].answer1;
+            getAnswer2.innerHTML = data.quizContent[control.count].answer2;
+            getAnswer3.innerHTML = data.quizContent[control.count].answer3;
+            getCurrentPoints.innerHTML = 'Points:' + ' ' + data.points;
+            this.newElement('button', 'submit', 'Submit Answer');
+        },
+        addAnswer: function(showMessage) {
+            var sumOfQuestions = data.quizContent.length;
+
+            if(showMessage === 'correct') {
+                this.newElement('p', 'showAnswer', 'Correct Answer!');
+            } else {
+                this.newElement('p', 'showAnswer', 'Incorrect Answer!');
+            }
+
+            if (control.count < sumOfQuestions - 1) {
+                this.newElement('button', 'nextQuest', 'Next question');
+            } else {
+                this.newElement('button', 'result', 'See your result');
+            }
+        },
+        removeAnswer: function(event) {
+            var getShowAnswer = document.getElementById('showAnswer');
+            var getShowAnswerParent = getShowAnswer.parentNode;
+            getShowAnswerParent.removeChild(getShowAnswer);
+            var clickedEl = event.target;
+            var clickedElParent = clickedEl.parentNode;
+            clickedElParent.removeChild(clickedEl);
+            var radioButtons = document.getElementsByName('answers');
+            var allRadioButtons = radioButtons.length;
+            var i;
+
+            for(i = 0; i < allRadioButtons; i++) {
+                radioButtons[i].checked = false;
+            }
+        },
+
+        // Displays final page of the quiz
+        resultPage: function() {
+            this.getApp.innerHTML = '<h1 class="h3">You have ' + data.points + ' question(s) answered correctly</h1>';
+            this.newElement('button', 'restart', 'Restart Quiz');
+        },
+        newElement: function(elem, elemId, elemText) {
+            var newElem = document.createElement(elem);
+            var newElemText = document.createTextNode(elemText);
+            newElem.appendChild(newElemText);
+            newElem.id = elemId;
+            this.getApp.appendChild(newElem);
+        }
+    };
+
+    var control = {
+        init: function() {
+            var start = document.getElementById('start') || document.getElementById('restart');
+            start.addEventListener('click', function() {
+                display.mainPage();
+                control.update();
+            }, false);
+        },
+        update: function() {
+            display.updateMainPage();
+            var submit = document.getElementById('submit');
+            submit.addEventListener('click', this.checkAnswer, false);
+        },
+
+        /**
+        * Alerts if none of the radios is checked on submit 
+        * Verifies correct/incorrect answer
+        * Directs quiz to the next question or to the final page
+        */
+        checkAnswer: function(event) {
+            var radioButtons = document.getElementsByName('answers'),
+                allRadioButtons = radioButtons.length,
+                isChecked = false,
+                checkedRadio,
+                clickedEl = event.target,
+                clickedElParent = clickedEl.parentNode,
+                i;
+
+            for (i = 0; i < allRadioButtons; i++) {
+                if (radioButtons[i].checked) {
+                    isChecked = true;
+                    checkedRadio = +radioButtons[i].value;
+                }
+            }
+
+            if (isChecked === false) {
+                alert('Please choose the answer!');
+            } else {
+                clickedElParent.removeChild(clickedEl);
+                if (checkedRadio === data.quizContent[control.count].correctAnswer) {
+                    display.addAnswer('correct');
+                    data.points++;
+                } else {
+                    display.addAnswer();
+                }
+
+                var nextQuestion = document.getElementById('nextQuest'),
+                    result = document.getElementById('result');
+
+                if (nextQuestion) {
+                    nextQuestion.addEventListener('click', function(event) {
+                        control.count++;
+                        display.removeAnswer(event);
+                        control.update();
+                    }, false);
+                } else {
+                    result.addEventListener('click', function() {
+                        display.resultPage();
+                        control.init();
+                        control.count = 0;
+                        data.points = 0;
+                    }, false);
+                }
+            }
+        },
+
+        // Used for incrementing/looping through the quiz questions and answers
+        count: 0
+    };
+
+    control.init();
+
+})();
